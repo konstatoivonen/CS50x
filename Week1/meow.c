12cfc53a863b17385f44d5847bd15409c7677314
Konsta Toivonen <konsta.toivonen@protonmail.com>
2022-12-01 12:09:42 +0200

    Working on Week1

--- Week1/meow.c
+++ Week1/meow.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
+void meow(int n);
+
 int main(void)
 {
-    int i = 0;
-    while (i < 3){
-        printf("Meow\n");
-        i++;
+     meow(3);
+}
+
+void meow(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("meow\n");
     }
-    
     
 }
